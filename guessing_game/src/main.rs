3eb27d41d66510731c3c73c619950a9fbec7351b
Buntin-Synthia <aokitakumi72@gmail.com>
2023-03-05 22:01:58 +0900

    imanotokoro

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,11 +4,7 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1..101);
-
-    println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
-
     loop {
         println!("Please input your guess.");
 
@@ -17,7 +13,10 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         println!("You guessed: {}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
