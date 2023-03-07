2fb0b960dd1267fc7dd2bc79d23d82110708fc9d
Buntin-Synthia <aokitakumi72@gmail.com>
2023-03-07 20:14:07 +0900

    yesbro

---
+++ functions/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    another_functions();
+}
+
+fn another_functions() {
+    println!("Hello, world!");
+}
--- itp1/src/main.rs
+++ itp1/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+fn main() {}
+
+fn read_line_sepated_integers() -> Vec<i32> {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    let v: Vec<i32> = s
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect();
+    return v;
 }
