f44c0c102dfaa491d046a4469282e1d6a171b025
Buntin-Synthia <aokitakumi72@gmail.com>
2023-03-13 20:57:04 +0900

    yes

--- itp1/src/main.rs
+++ itp1/src/main.rs
@@ -1,48 +1,15 @@
 fn main() {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).unwrap();
-    //input n,m,l
-    let mut iter = s.split_whitespace();
-    let n: usize = iter.next().unwrap().parse().unwrap();
-    let m: usize = iter.next().unwrap().parse().unwrap();
-    let l: usize = iter.next().unwrap().parse().unwrap();
-    //input matrix A
-    let mut a: Vec<Vec<i64>> = vec![vec![0; m]; n];
-    for i in 0..n {
-        let mut s = String::new();
-        std::io::stdin().read_line(&mut s).unwrap();
-        let mut iter = s.split_whitespace();
-        for j in 0..m {
-            a[i][j] = iter.next().unwrap().parse().unwrap();
-        }
-    }
-    //input matrix B
-    let mut b = vec![vec![0; l]; m];
-    for i in 0..m {
-        let mut s = String::new();
-        std::io::stdin().read_line(&mut s).unwrap();
-        let mut iter = s.split_whitespace();
-        for j in 0..l {
-            b[i][j] = iter.next().unwrap().parse().unwrap();
-        }
-    }
-    //culc matrix C
-    let mut c = vec![vec![0; l]; n];
-    for i in 0..n {
-        for j in 0..l {
-            for k in 0..m {
-                c[i][j] += a[i][k] * b[k][j];
+    let out = s
+        .chars()
+        .map(|c| {
+            if c.is_lowercase() {
+                c.to_uppercase().to_string()
+            } else {
+                c.to_lowercase().to_string()
             }
-        }
-    }
-    //output matrix C
-    for i in 0..n {
-        for j in 0..l {
-            if j != 0 {
-                print!(" ");
-            }
-            print!("{}", c[i][j]);
-        }
-        println!("");
-    }
+        })
+        .collect::<String>();
+    print!("{}", out);
 }
